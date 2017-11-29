1cf35e148d38b9dec921946232a3946d1483fce3
Diego Melgar <dlopez.project.sv@gmail.com>
2017-11-28 19:33:18 -0600

    adding logic to store vuex

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,3 +1,28 @@
-export default {
+import _ from 'underscore'
 
+export default {
+  addPlayer: (state, payload) => {
+    let player = state.players.find((p) => p.account === payload.account)
+
+    if (typeof player === 'undefined') {
+      state.players.push({
+        name: payload.name,
+        account: payload.account
+      })
+    }
+  },
+  removePlayer: (state, account) => {
+    let player = state.players.find(p => p.account === account)
+
+    if (player) {
+      state.players.splice(state.players.indexOf(player.account))
+    }
+  },
+  shufflePlayers: (state) => {
+    state.result = _.shuffle(state.players)
+    state.showPlayers = false
+  },
+  shuffleAgain: (state) => {
+    state.result = _.shuffle(state.result)
+  }
 }
--- src/store/state.js
+++ src/store/state.js
@@ -1,3 +1,10 @@
 export default {
-
+  player: {
+    name: '',
+    account: ''
+  },
+  players: [],
+  result: [],
+  showPlayers: true,
+  gameAmount: 0,
 }
